51bac5f7e803fed6c958b4eca2705bd78f4c686e
robynforrest <robynforrest@gmail.com>
2022-10-21 13:15:57 -0700

    add code to calc selectivity

---
+++ get-sel-pars.R
@@ -0,0 +1,62 @@
+# Get selectivity parameters
+# Robyn Forrest Oct 21 2022
+# Estimate the logistic selectivity parameters given selectivity at age
+
+library(tidyverse)
+library(reshape2)
+
+# data
+data <- readRDS("Ex2data.rda")
+data <- data$BI
+obs <- data$sel
+obs[8:length(obs)] <- 1
+age <- 1:length(obs)
+
+# parameters
+ah <- 3
+sig <- 0.2
+pars <- c(ah,sig)
+
+# model
+get.sel <- function(pars){
+  
+  #model
+  ah=pars[1]
+  sig=pars[2]
+  
+  pred <- 1/(1+exp(-(age-ah)/sig)) # this is the same as pred <- plogis(age,ah,sig)
+  
+  #likelihood (just a simple sum of squares)
+  like <- sum((obs-pred)^2)	
+  
+  #output
+  output=list()
+  output$like=like
+  output$pred=pred
+  return(output)
+}
+
+tmp <- get.sel(pars)
+tmp$pred
+
+fn=function(pars) {get.sel(pars)$like}
+
+fit = optim(pars,fn, method="BFGS", hessian=F)
+fit$par
+
+Predicted <- get.sel(fit$par)$pred
+
+# plot
+D <- cbind(age,Predicted, obs) %>%
+  as.data.frame() %>% 
+  rename(Observed=obs) %>% 
+  melt(id="age", value.name="Selectivity")
+  
+p <- ggplot(D)+
+  geom_path(aes(x=age,y=Selectivity,colour=variable,linetype=variable), size=1.5)+
+  theme_classic()
+print(p)
+
+
+
+
